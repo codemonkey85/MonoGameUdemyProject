a553cd68c28ba93463b6f1f5b04f77512bb445be
Michael Bond <michael@bondcodes.com>
2025-01-15 13:45:19 -0500

    Add motion

--- IntroGraphics/MonoIntroGame1.cs
+++ IntroGraphics/MonoIntroGame1.cs
@@ -10,29 +10,25 @@ public class MonoIntroGame1 : Game
     private SpriteBatch spriteBatch;
 
     private Texture2D daffySpriteTexture;
-    private Texture2D cartoonSpriteTexture;
+
+    private Vector2 daffyPosition = new(0, 0);
+
+    // add a velocity variable
+    private Vector2 daffyVelocity = new(5, 1);
 
     public MonoIntroGame1()
     {
         graphics = new(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
-    }
-
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
-
-        base.Initialize();
+        Window.Position = new(400, 200);
     }
 
     protected override void LoadContent()
     {
         spriteBatch = new(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
         daffySpriteTexture = Content.Load<Texture2D>("images/daffy");
-        cartoonSpriteTexture = Content.Load<Texture2D>("images/cartoon");
     }
 
     protected override void Update(GameTime gameTime)
@@ -43,7 +39,16 @@ public class MonoIntroGame1 : Game
             Exit();
         }
 
-        // TODO: Add your update logic here
+        daffyPosition += daffyVelocity;
+        if (daffyPosition.X + daffySpriteTexture.Width > graphics.GraphicsDevice.Viewport.Width || daffyPosition.X < 0)
+        {
+            daffyVelocity.X *= -1;
+        }
+
+        if (daffyPosition.Y + daffySpriteTexture.Height > graphics.GraphicsDevice.Viewport.Height || daffyPosition.Y < 0)
+        {
+            daffyVelocity.Y *= -1;
+        }
 
         base.Update(gameTime);
     }
@@ -52,12 +57,10 @@ public class MonoIntroGame1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
         spriteBatch.Begin();
-        
-        spriteBatch.Draw(daffySpriteTexture, new Vector2(0, 0), Color.White);
-        spriteBatch.Draw(cartoonSpriteTexture, new Vector2(400, 200), Color.White);
-        
+
+        spriteBatch.Draw(daffySpriteTexture, daffyPosition, Color.White);
+
         spriteBatch.End();
 
         base.Draw(gameTime);
